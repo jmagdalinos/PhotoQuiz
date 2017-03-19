e3c7d55ab20084b8c6bd39393170e4240a2335f2
John Magdalinos <jmagdalinos@gmail.com>
2017-03-19 17:40:30 +0100

    Initialization of Views no in onCreate Fixed orientation problems

--- app/src/main/java/com/example/android/photoquiz/MainActivity.java
+++ app/src/main/java/com/example/android/photoquiz/MainActivity.java
@@ -24,27 +24,55 @@ import android.widget.TextView;
  * This app is a photography quiz which returns a score to the user
  */
 public class MainActivity extends AppCompatActivity {
-    //Declare variables and prepare to save instance
-    int finalScore = 0;
+    //Declare variables
+    int finalScore;
     final int[] RES_ID = {100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127};
     final int[] ANSWERS = {0,0,1,0,1,0,0,0,1,0,1,0,0,1,0,1,0,1,0,0,1,1,0,0,1,0,0,1};
     int[] userInput = new int[28];
     int lastIndexUsed = 0;
     int[] editTextAnswers = {0,0,0};
+    int enableSubmitButtonSwitch = 0; //enableSubmitButtonSwitch = 0 --> Submit button has not been pressed
+    int enableShowAnswersSwitch = 0; //enableShowAnswersSwitch = 0 --> Show Answers button has not been pressed
 
+    //Declare Views
+    ViewGroup viewGroup;
+    TextView finalScoreTextView;
+    Button answersButton;
+    Button submitButton;
+    EditText question4EditText;
+    EditText question6EditText;
+    EditText question8EditText;
+
+
+    //Prepare to save instance
     private static final String STATE_FINAL_SCORE = "finalScore";
     private static final String STATE_USER_INPUT = "userInput";
     private static final String STATE_EDIT_TEXT_ANSWERS = "editTextAnswers";
+    private static final String STATE_ENABLE_SUBMIT_SWITCH = "enableSubmitButtonSwitch";
+    private static final String STATE_ENABLE_SHOW_ANSWERS_SWITCH = "enableShowAnswersSwitch";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        finalScore = 0;
+
+        //Initialize Views
+        initializeViews();
+
         initArray();
         assignResIds();
         displayScore();
-        disableAnswersButton();
+
+        if (enableSubmitButtonSwitch == 1) {
+            disableSubmitButton();
+            enableAnswersButton();
+        } else if (enableSubmitButtonSwitch == 0) {
+            enableSubmitButton();
+            disableAnswersButton();
+        }
+
     }
 
     @Override
@@ -52,6 +80,8 @@ public class MainActivity extends AppCompatActivity {
         savedInstanceState.putInt(STATE_FINAL_SCORE, finalScore);
         savedInstanceState.putIntArray(STATE_USER_INPUT, userInput);
         savedInstanceState.putIntArray(STATE_EDIT_TEXT_ANSWERS, editTextAnswers);
+        savedInstanceState.putInt(STATE_ENABLE_SUBMIT_SWITCH, enableSubmitButtonSwitch);
+        savedInstanceState.putInt(STATE_ENABLE_SHOW_ANSWERS_SWITCH, enableShowAnswersSwitch);
 
         //Always call the superclass so it can save the view hierarchy state
         super.onSaveInstanceState(savedInstanceState);
@@ -65,9 +95,22 @@ public class MainActivity extends AppCompatActivity {
         finalScore = savedInstanceState.getInt(STATE_FINAL_SCORE);
         userInput = savedInstanceState.getIntArray(STATE_USER_INPUT);
         editTextAnswers = savedInstanceState.getIntArray(STATE_EDIT_TEXT_ANSWERS);
-
+        enableSubmitButtonSwitch = savedInstanceState.getInt(STATE_ENABLE_SUBMIT_SWITCH);
+        enableShowAnswersSwitch = savedInstanceState.getInt(STATE_ENABLE_SHOW_ANSWERS_SWITCH);
         //Display variables
         displayScore();
+
+        if (enableSubmitButtonSwitch == 1) {
+            disableSubmitButton();
+            enableAnswersButton();
+        } else if (enableSubmitButtonSwitch == 0) {
+            enableSubmitButton();
+            disableAnswersButton();
+        }
+
+        if (enableShowAnswersSwitch == 1) {
+            answersButton.performClick();
+        }
     }
 
     public void submitScoreButton (View view) {
@@ -78,10 +121,13 @@ public class MainActivity extends AppCompatActivity {
 
         //Displays score, enables "Show Results" Button, disables "Submit Your Score!" Button
         Log.v("MainActivity", "Score: " + finalScore);
+        calcScore();
         displayScore();
         enableAnswersButton();
         disableSubmitButton();
         disableViews();
+
+        enableSubmitButtonSwitch = 1;
 
     }
 
@@ -120,6 +166,8 @@ public class MainActivity extends AppCompatActivity {
             view4.setBackgroundColor(Color.rgb(229, 115, 115));
             view4.setText("Guide Number");
         }
+
+        enableShowAnswersSwitch = 1;
     }
 
     public void resetButton (View view) {
@@ -132,6 +180,9 @@ public class MainActivity extends AppCompatActivity {
         disableAnswersButton();
         enableViews();
         resetBackgroundColors();
+
+        enableSubmitButtonSwitch = 0;
+        enableShowAnswersSwitch = 0;
     }
 
 
@@ -149,7 +200,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Assigns Resource Ids to all CheckBoxes and RadioButtons from resId array
     public void assignResIds () {
-        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.questions);
+//        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.questions);
         int counterViews = viewGroup.getChildCount();
 
         for (int i = 0; i < counterViews; i++) {
@@ -431,7 +482,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Checks EditText Answers
     public void checkQuestion4() {
-        EditText question4EditText = (EditText) findViewById(R.id.question_4_Edit_Text);
+//        EditText question4EditText = (EditText) findViewById(R.id.question_4_Edit_Text);
         String question4Answer = question4EditText.getText().toString();
         question4Answer = question4Answer.toUpperCase();
 
@@ -441,7 +492,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void checkQuestion6() {
-        EditText question6EditText = (EditText) findViewById(R.id.question_6_Edit_Text);
+//        EditText question6EditText = (EditText) findViewById(R.id.question_6_Edit_Text);
         String question6Answer = question6EditText.getText().toString();
         question6Answer = question6Answer.toUpperCase();
 
@@ -451,7 +502,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void checkQuestion8() {
-        EditText question8EditText = (EditText) findViewById(R.id.question_8_Edit_Text);
+//        EditText question8EditText = (EditText) findViewById(R.id.question_8_Edit_Text);
         String question8Answer = question8EditText.getText().toString();
         question8Answer = question8Answer.toUpperCase();
 
@@ -461,7 +512,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    //Finds index of Resource Id in resID array
+    //Finds index of Resource Id in resID array and assign value in userInput array based on user's answer
     public void resIdIndexChecked(int number) {
         for (int i = 0; i < 28; i ++) {
             if (RES_ID[i] == number) {
@@ -474,7 +525,6 @@ public class MainActivity extends AppCompatActivity {
     public void resIdIndexUnchecked(int number) {
         for (int i = 0; i < 28; i ++) {
             if (RES_ID[i] == number) {
-                Log.v("MainActivity",number + " " + i);
                 userInput[i] = 0;
                 break;
             }
@@ -482,8 +532,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    //Display score
-    public void displayScore () {
+    //Calculate score
+    public void calcScore () {
         for (int i = 0; i < 28; i ++) {
             if (userInput[i] == ANSWERS[i] && ANSWERS[i] == 1){
                 finalScore += 2;
@@ -495,8 +545,12 @@ public class MainActivity extends AppCompatActivity {
                 finalScore += 6;
             }
         }
+    }
 
-        TextView finalScoreTextView = (TextView) findViewById(R.id.score_Box);
+
+    //Display score
+    public void displayScore () {
+//        TextView finalScoreTextView = (TextView) findViewById(R.id.score_Box);
         finalScoreTextView.setText("" + finalScore + "/40");
     }
 
@@ -504,25 +558,31 @@ public class MainActivity extends AppCompatActivity {
     //Enable/Disable Buttons & Views
     public void enableAnswersButton() {
         //Enable "Show Answers" button
-        Button enableAnswersButton = (Button) findViewById(R.id.answers_Button);
-        enableAnswersButton.setEnabled(true);
+//        Button enableAnswersButton = (Button) findViewById(R.id.answers_Button);
+        answersButton.setEnabled(true);
     }
 
     public void disableAnswersButton () {
         //Disable "Show Answers" button
-        Button disableAnswersButton = (Button) findViewById(R.id.answers_Button);
-        disableAnswersButton.setEnabled(false);
+//        Button disableAnswersButton = (Button) findViewById(R.id.answers_Button);
+        answersButton.setEnabled(false);
     }
 
     public void enableSubmitButton () {
         //Enable "Submit Your Score!" button
-        Button disableSubmitButton = (Button) findViewById(R.id.submit_Button);
-        disableSubmitButton.setEnabled(true);
+//        Button disableSubmitButton = (Button) findViewById(R.id.submit_Button);
+        submitButton.setEnabled(true);
+    }
+
+    public void disableSubmitButton () {
+        //Disable "Submit Your Score!" button
+//        Button disableSubmitButton = (Button) findViewById(R.id.submit_Button);
+        submitButton.setEnabled(false);
     }
 
     public void disableViews () {
         //Disables all CheckBoxes, RadioButtons and EditViews
-        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.questions);
+//        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.questions);
         int counterViews = viewGroup.getChildCount();
 
         for (int i = 0; i < counterViews; i++) {
@@ -552,7 +612,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void enableViews () {
         //Enables all CheckBoxes, RadioButtons and EditViews and resets them
-        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.questions);
+//        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.questions);
         int counterViews = viewGroup.getChildCount();
 
         for (int i = 0; i < counterViews; i++) {
@@ -581,12 +641,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void disableSubmitButton () {
-        //Disable "Submit Your Score!" button
-        Button disableSubmitButton = (Button) findViewById(R.id.submit_Button);
-        disableSubmitButton.setEnabled(false);
-    }
-
 
     //Resets background colors
     public void resetBackgroundColors () {
@@ -596,12 +650,24 @@ public class MainActivity extends AppCompatActivity {
             view1.setBackgroundColor(Color.TRANSPARENT);
         }
 
-        View view2 = findViewById(R.id.question_4_Edit_Text);
-        View view3 = findViewById(R.id.question_6_Edit_Text);
-        View view4 = findViewById(R.id.question_8_Edit_Text);
+//        View view2 = findViewById(R.id.question_4_Edit_Text);
+//        View view3 = findViewById(R.id.question_6_Edit_Text);
+//        View view4 = findViewById(R.id.question_8_Edit_Text);
 
-        view2.setBackgroundColor(Color.TRANSPARENT);
-        view3.setBackgroundColor(Color.TRANSPARENT);
-        view4.setBackgroundColor(Color.TRANSPARENT);
+        question4EditText.setBackgroundColor(Color.TRANSPARENT);
+        question6EditText.setBackgroundColor(Color.TRANSPARENT);
+        question8EditText.setBackgroundColor(Color.TRANSPARENT);
+    }
+
+    // Initializes all views
+    public void initializeViews() {
+        answersButton = (Button) findViewById(R.id.answers_Button);
+        submitButton = (Button) findViewById(R.id.submit_Button);
+        viewGroup = (ViewGroup) findViewById(R.id.questions);
+        finalScoreTextView = (TextView) findViewById(R.id.score_Box);
+        question4EditText = (EditText) findViewById(R.id.question_4_Edit_Text);
+        question6EditText = (EditText) findViewById(R.id.question_6_Edit_Text);
+        question8EditText = (EditText) findViewById(R.id.question_8_Edit_Text);
+
     }
 }
